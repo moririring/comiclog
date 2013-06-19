ebee10085b469adcc03c6f4ec7636f54f0c7318b
moriri ring <moriri.ring10@gmail.com>
2013-06-19 19:11:35 +0900

    add

---
+++ app/models/comic.rb
@@ -0,0 +1,3 @@
+class Comic < ActiveRecord::Base
+  attr_accessible :author, :comic_name, :id, :publisher
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comiclog::Application.config.secret_token = '1f7a7a4c83bd6bdcc5ed0f243ddaa2cd8f4787d0b8a4b606a8e813296ed9c7c8c5c0ce0ac25e437df0f7143c4caa9d42d275b728dfe9840068cbed17be5f8c15'
