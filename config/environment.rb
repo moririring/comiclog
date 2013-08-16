cabaf5567751fd6945081d19614fbc966fa27fa8
moriri ring <binmatsui@hotmail.com>
2013-08-17 03:00:26 +0900

    http://penguin-web.dyndns.org/mountain/blogs/406 これを試す

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+require 'rails_file_column'
 # Initialize the rails application
 Comiclog::Application.initialize!
