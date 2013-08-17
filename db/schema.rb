e7f0f5c1b113958ca945ae8dcf6d09606dfc6f67
moriri ring <binmatsui@hotmail.com>
2013-08-17 15:16:34 +0900

    nullはいらない

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(:version => 20130816161650) do
     t.text     "link"
     t.text     "img"
     t.text     "content"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
